32203dc0b05fcaca91cee350d681c2b870c995b6
Serene PSL <serenepslkenya@gmail.com>
2023-10-14 11:48:15 +0300

    damn

---
+++ components/areachart.jsx
@@ -0,0 +1,70 @@
+import ReactECharts from "echarts-for-react";
+import { useEffect, useState } from "react";
+
+export default function AreaChart({ data }) {
+  const [option, setOption] = useState({});
+
+  useEffect(() => {
+    const chartOptions = {
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+      xAxis: {
+        axisLine: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+        data: ["12am", "4am", "8am", "12pm", "4pm", "8pm", "12am"],
+      },
+      yAxis: {
+        alignTicks: true,
+        axisLine: {
+          show: false,
+        },
+      },
+      series: [
+        {
+          name: "Solar production",
+          tooltip: {
+            valueFormatter: (value) => value + " kW",
+          },
+          data,
+          type: "line",
+          areaStyle: {
+            color: "#00ff00",
+            opacity: 0.2,
+          },
+          lineStyle: {
+            normal: {
+              color: "green",
+            },
+          },
+        },
+        {
+          name: "Home Usage",
+          tooltip: {
+            valueFormatter: (value) => value + " kW",
+          },
+          data: [25, 14, 23, 35, 10, 5, 3],
+          type: "line",
+          areaStyle: {
+            color: "#ff0",
+            opacity: 0.2,
+          },
+          lineStyle: {
+            normal: {
+              color: "#ff0",
+            },
+          },
+        },
+      ],
+    };
+    setOption(chartOptions);
+  }, []);
+  return <ReactECharts style={{ height: "85vh" }} option={option} />;
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,39 @@
 import Image from "next/image";
 import myGif from "../public/solaronly.gif";
-import { Avatar, Badge } from "@mantine/core";
+import { Avatar, Badge, Text } from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
 import { useState } from "react";
-import BottomNavigation from "../components/BottomNavigation";
-import {
-  IconHome,
-  IconAdjustments,
-  IconChartAreaLine,
-  IconNotification,
-  IconChartSankey,
-  IconMessageDots,
-} from "@tabler/icons";
+import dynamic from "next/dynamic";
+
+import ReactFullpage from "@fullpage/react-fullpage";
+
+const DynamicAreaChart = dynamic(() => import("../components/areachart"), {
+  loading: () => <p>Loading...</p>,
+});
+
+const Fullpage = () => (
+  <ReactFullpage
+    scrollingSpeed={1000} /* Options here */
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <Dashboard />
+          </div>
+          <div className="section">
+            <Analytics />
+          </div>
+          <div className="section">
+            <Notifications />
+          </div>
+          <div className="section">
+            <Settings />
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
 
 const Dashboard = () => {
   const [range, setRange] = useState([null, null]);
@@ -43,14 +65,56 @@ const Dashboard = () => {
           maw={400}
         />
 
-        <Image src={myGif} alt="my gif" height={500} width={500} />
+        <div className="relative mt-24">
+          <hr className="rotate-90 w-[80px] absolute top-[60px]" />
+          <div className="absolute top-[-60px] left-[20px]">
+            <Text color="green" fz={"2rem"}>
+              4.0 kW
+            </Text>
+            <Text fw="lighter">SOLAR PRODUCTION</Text>
+          </div>
+
+          <hr className="rotate-90 w-[200px] absolute top-[150px] left-[170px]" />
+          <div className="absolute top-[-30px] left-[220px]">
+            <Text color="orange" fz={"2rem"}>
+              2.0 kW
+            </Text>
+            <Text fw="lighter">HOME USAGE</Text>
+          </div>
+
+          <hr className="rotate-90 w-[50px] absolute top-[362px] left-[230px]" />
+          <div className="absolute top-[390px] left-[220px]">
+            <Text color="white" fz={"2rem"}>
+              2.0 kW
+            </Text>
+            <Text fw="lighter">TO GRID </Text>
+          </div>
+
+          <hr className="rotate-90 w-[80px] absolute top-[360px] left-[50px]" />
+          <div className="absolute top-[400px] left-[60px]">
+            <Text color="blue" fz={"2rem"}>
+              2.0 kWh
+            </Text>
+            <Text fw="lighter">
+              <strong className="text-green-600">80%</strong> capacity
+            </Text>
+            <Text fw="lighter">BATTERY</Text>
+          </div>
+
+          <Image src={myGif} alt="my gif" height={500} width={500} />
+        </div>
       </div>
     </div>
   );
 };
 
 const Analytics = () => {
-  return <div>analytics</div>;
+  return (
+    <div>
+      <h1 className="ml-4">Analytics</h1>
+      <DynamicAreaChart data={[0, 0, 5, 22, 27, 0, 0]} />
+    </div>
+  );
 };
 
 const Notifications = () => {
@@ -61,33 +125,10 @@ const Settings = () => {
   return <div>settings</div>;
 };
 
-const sections = [
-  {
-    key: "1",
-    icon: <IconHome />,
-    component: <Dashboard />,
-  },
-  {
-    key: "2",
-    icon: <IconChartSankey />,
-    component: <Analytics />,
-  },
-  {
-    key: "3",
-    icon: <IconMessageDots />,
-    component: <Notifications />,
-  },
-  {
-    key: "4",
-    icon: <IconAdjustments />,
-    component: <Settings />,
-  },
-];
-
 export default function Home() {
   return (
     <div>
-      <BottomNavigation sections={sections} activeColor="green" />
+      <Fullpage />
     </div>
   );
 }
